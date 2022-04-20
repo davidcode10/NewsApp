46d9dcd0bf228ac2700c3b06a74c9ac2883a9fc1
davidcode10 <davidedson2030@outlook.com>
2022-04-20 14:16:53 +0200

    NewsApp created

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by David Santiago on 20/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
